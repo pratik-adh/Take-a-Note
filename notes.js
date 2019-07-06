ced727e87494727c81b1037abd0cee70696f2c6a
pratikadh <31108938+pratikadh@users.noreply.github.com>
2019-07-06 14:15:32 +0545

    Update notes.js

--- notes.js
+++ notes.js
@@ -1,20 +1,19 @@
 const fs = require('fs')
 const chalk = require('chalk')
-const getNotes = function(){
+
+const getNotes = () => {
     return 'Your Notes...'
 }
 
-const addNote = function(title,body){
+const addNote = (title,body) =>{
     const notes = loadNotes()
     //Array filter method to check whether
     //the JOSN contains duplicate data or not
 
-    const duplicateNotes = notes.filter(function(notes){
-        return notes.title === title
-    })
+    //const duplicateNotes = notes.filter((note) => note.title === title)
+    const duplicateNote = notes.find((note) => note.title === title)  
 
-    if(duplicateNotes.length === 0){ // if duplicate data are not found
-        //pushing object with title and body in json file
+    if(!duplicateNote){
         notes.push({
             title: title,
             body: body
@@ -24,14 +23,27 @@ const addNote = function(title,body){
     }else{
         console.log('Note Title Taken')
     }
+
+    // if(duplicateNotes.length === 0){ // if duplicate data are not found
+    //     //pushing object with title and body in json file
+    //     notes.push({
+    //         title: title,
+    //         body: body
+    //     })
+    //     saveNotes(notes)
+    //     console.log('New note added')
+    // }else{
+    //     console.log('Note Title Taken')
+    // }
+    
 }
 //now we need to save the notes
-const saveNotes = function(notes){
+const saveNotes = (notes) => {
     const dataJSON = JSON.stringify(notes)
     fs.writeFileSync('notes.json', dataJSON)
 }
 
-const loadNotes = function(){
+const loadNotes = () =>{
     try{
         const dataBuffer = fs.readFileSync('notes.json')
         const dataJSON = dataBuffer.toString()
@@ -41,12 +53,10 @@ const loadNotes = function(){
     }
 }
 
-const removeNote = function(title){
+const removeNote = (title) => {
     const notes = loadNotes()
 
-    const NotestoKeep = notes.filter(function(notes){
-        return notes.title !== title
-    })
+    const NotestoKeep = notes.filter((notes) => notes.title !== title)
 
     if(notes.length > NotestoKeep.length){ // if duplicate data are not found
         //pushing object with title and body in json file
@@ -57,8 +67,28 @@ const removeNote = function(title){
     }
     saveNotes(NotestoKeep)
 }
+const listNotes = function(){
+    const notes = loadNotes()
+    
+    notes.forEach((note) => {
+        console.log(note.title)
+    })
+}
+const readNotes = (title) =>{
+    const notes = loadNotes()
+    const note = notes.find((note) => note.title === title)
+
+    if(note){
+        console.log(chalk.red(note.title))
+        console.log(note.body)
+    }else{
+        console.log('Note not found.')
+    }
+}
 module.exports = {
     getNotes: getNotes,
     addNote: addNote,
-    removeNote: removeNote
+    removeNote: removeNote,
+    listNotes: listNotes,
+    readNotes: readNotes
 }
